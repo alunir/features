49bab206e691a83e3cf9890ed9ca4a569434f928
tk42 <nsplat@gmail.com>
2024-11-28 18:02:45 +0900

    Refactor main.py to dynamically set the port for uvicorn.run

--- prefect/main.py
+++ prefect/main.py
@@ -431,4 +431,5 @@ def root(resol: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
